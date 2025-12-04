2fdb2f67b1b0371a9aa70ee791692e9a3f8393a7
Radhik Ampani <158242842+pyRadhik@users.noreply.github.com>
2025-12-04 10:07:03 +0100

    Update user.py

--- users/user.py
+++ users/user.py
@@ -10,12 +10,12 @@ class User():
         self.hashed_id = hashed_id
         self.private_key = private_key
     
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other: Any):
         #hashed_id verification
         if not isinstance(other, User):
             return False
         return self.hashed_id == other.hashed_id
     
-    def __hash__(self) -> int:
+    def __hash__(self):
         #Generate hash based on id
         return hash(self.hashed_id)
