dcb055c1f7f1f8172d71f94fcc318ca8a263635a
Radhik Ampani <158242842+pyRadhik@users.noreply.github.com>
2025-12-09 09:59:18 +0100

    Update user_manager.py

--- users/user_manager.py
+++ users/user_manager.py
@@ -9,17 +9,17 @@ class UserManager:
 
     def add_user(self, user: User):
         if user.name in self.users:
-            raise Exception(f"User with name '{user.name}' already exists")
+            raise Exception("User with name " + user.name + (" already exists")
         self.users[user.name] = user
 
     def delete_user(self, name: str):
         if name not in self.users:
-            raise Exception(f"User with name '{name}' not found")
+            raise Exception("User with name " + name + "not found")
         del self.users[name]
 
     def update_user(self, name: str, email: str = None, hashed_id: str = None, private_key: str = None):
         if name not in self.users:
-            raise Exception(f"User with name '{name}' not found")
+            raise Exception("User with name " + name + "not found")
         
         user = self.users[name]
 
